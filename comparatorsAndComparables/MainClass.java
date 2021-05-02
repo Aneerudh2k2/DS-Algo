263421f2e7517b0c853a7fb8d8e37b18d5c40700
Aneerudh2k2 <63917783+Aneerudh2k2@users.noreply.github.com>
2021-05-02 17:32:21 +0530

    Update MainClass.java

--- comparatorsAndComparables/MainClass.java
+++ comparatorsAndComparables/MainClass.java
@@ -39,7 +39,7 @@ public class MainClass {
 			@Override
 			public int compare(Student o1, Student o2) {
 				if (o1.name.equals(o2.name)) {
-					return o1.marks - o2.marks;
+					return o1.marks - o2.marks; // if -1 sort ascending, if +1 sort descending, if 0 already sorted
 				} else {
 					return o1.name.compareTo(o2.name);
 				}
